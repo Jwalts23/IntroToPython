2346eb428d11038f7e9b834e09854500377bb039
Jwalts23 <bwbella0123@gmail.com>
2024-01-08 14:24:53 -0500

    basic hi low with guess tracking

---
+++ guesses.py
@@ -0,0 +1,51 @@
+import array
+
+class GuessController:
+    
+    def __init__(self, permittedGuesses):
+        self.guess = 0
+        self.guesses = array.array("i")
+        self.permittedGuesses = permittedGuesses
+
+
+    def printGuesses(self):
+        if self.guess > 0:
+            print("Your Guesses are:", end=" ")
+            for i in range(self.guess):
+                g = self.guesses[i]
+                if i < (self.guess-1):
+                    print(g, end=", ")
+                else:
+                    print(g)
+
+    
+    def recordGuess(self, guess):
+        self.guesses.append(guess)
+        self.guess +=1
+    
+    def reset(self):
+        self.guess = 0
+        self.guesses = array.array("i")
+
+    def isPreviousGuessTheSame(self, guess):
+        if self.guess > 0:
+            if guess == self.guesses[self.guess-1]:
+                return True
+            else :
+                return False
+        return False
+
+    def remainingGuesses(self):
+        return self.permittedGuesses - self.guess
+    
+# gc = GuessController(15)
+
+# gc.printGuesses()
+# gc.recordGuess(15)
+# gc.recordGuess(30)
+# gc.recordGuess(30)
+# gc.recordGuess(22)
+# gc.recordGuess(30)
+# print("Guess is same as last? "+ str(gc.isPreviousGuessTheSame(22)))
+# gc.printGuesses()
+# print("Remaining Guesses: "+ str(gc.remainingGuesses()))
--- hiLow.py
+++ hiLow.py
@@ -1,1 +1,23 @@
 # This will become a highlow game sooooonnnn
+
+import random
+from guesses import GuessController
+
+gc = GuessController(10)
+
+number = random.randint(0,100)
+guessing = True
+while guessing:
+    gc.printGuesses()
+    guess = int(input("Enter your guess here: "))
+
+    if guess > number:
+        print("Too High")
+
+    elif guess < number:
+        print("Too Low")
+
+    elif number == guess:
+        print("You did it! The number was " + str(number))
+        guessing = False
+    gc.recordGuess(guess)
