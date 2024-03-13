7f7ff6f12aa0ada9d215397ba75420ca1f6d8e9e
Jolie Walts <81712807+Jwalts23@users.noreply.github.com>
2024-03-13 15:16:40 -0400

    Regular hi low updated to run as an exe

--- hiLow.py
+++ hiLow.py
@@ -2,11 +2,44 @@
 
 import random
 from guesses import GuessController
+import getopt, sys
 
+# Remove 1st argument from the
+# list of command line arguments
+argumentList = sys.argv[1:]
+print("arguments list " , argumentList)
 gc = GuessController(10)
+lowerLimit = 0
+upperLimit = 100
 
-# global number, askingToPlayAgain, guessing
-number = random.randint(0,100)
+
+# Options
+options = "l:u:h"
+# Long options
+long_options = ["Help", "Lower=", "Upper="]
+
+try:
+    # Parsing argument
+    arguments, values = getopt.getopt(argumentList, options, long_options)
+    # checking each argument
+    for currentArgument, currentValue in arguments:
+        if currentArgument in ("-h", "--Help"):
+            print("-u | --Upper  upper limit value")
+            print("-l | --Lower  lower limit value")
+
+        elif currentArgument in ("-l", "--Lower"):
+            print("Lower Limit:", currentValue)
+            lowerLimit = int(float(currentValue))
+
+        elif currentArgument in ("-u", "--Upper"):
+            print("Upper Limit:", currentValue)
+            upperLimit = int(currentValue)
+
+except getopt.error as err:
+    # output error, and return with an error code
+    print(str(err))
+
+number = random.randint(lowerLimit, upperLimit)
 guessing = True
 askingToPlayAgain = True
 
@@ -16,7 +49,7 @@ def playAgain():
         playAgain = input("Would you like to play again?  ")
         if playAgain == "Y" or playAgain == "y":
             gc.reset()
-            number = random.randint(0,100)
+            number = random.randint(lowerLimit,upperLimit)
             guessing = True
             askingToPlayAgain = False
         elif playAgain == "N" or playAgain == "n":
@@ -33,7 +66,7 @@ while guessing:
         gc.printGuesses()
         
         try:
-            guess = int(input("Enter a number between 0-100: "))
+            guess = int(input("Enter a number between " + str(lowerLimit) + "-" + str(upperLimit) + ": "))
             if not gc.isPreviousGuessTheSame(guess):
                 if guess > number:
                     print("Too High")
